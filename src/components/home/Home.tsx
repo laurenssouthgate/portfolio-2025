310a3062240324ac8da0820a4d029770f3723204
Laurens <laurenssouthgate@gmail.com>
2025-05-16 09:47:15 +0100

    added content to home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Float, Environment } from "@react-three/drei";
 import CerealBox from "./CerealBox";
+import "./Home.css";
+import { ContentContext } from "../../context/ContentContext";
+import { useContext } from "react";
+import { Content } from "../../model/Content";
 
 const Home = () => {
+    const { setContent } = useContext(ContentContext);
+
     return (
         <div className="home-content full">
+            <div className="hero-section">
+                <h1>Front-end developer blending technical skill with creative vision.</h1>
+                <button onClick={ () => setContent(Content.Projects) }>
+                    Check out my projects
+                </button>
+            </div>
             <Canvas
                 className="canvas-root"
                 style={{
@@ -15,17 +27,16 @@ const Home = () => {
                     height: '100vh',
                     zIndex: 0,
                 }}
-                camera={{ position: [1, 2, 4], fov: 50 }}
-            >
+                camera={{ position: [1, 2, 4], fov: 50 }}>
                 <Environment preset="sunset" />
-                <Float floatIntensity={0.2} speed={1.75} rotationIntensity={0.5}>
+                <Float floatIntensity={ 0.2 } speed={ 1.75 } rotationIntensity={ 0.5 }>
                     <CerealBox 
                         position={[0, -1.5, 0]}
                         rotation={[0,-3.6, 0]}
                         scale={[1, 1, 1]}
                     />
                 </Float>
-                <OrbitControls enableZoom={false} target={[0, 0, 0]} />
+                <OrbitControls enableZoom={ false } target={[0, 0, 0]} />
             </Canvas>
         </div>
     )
