551fbeec14bdae6818546682c29ab580c5afa2bf
Laurens <laurenssouthgate@gmail.com>
2025-05-21 20:52:30 +0100

    possible fix for nav

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    },
+    postcss: {
+      plugins: []
+    }
+  },
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 })
